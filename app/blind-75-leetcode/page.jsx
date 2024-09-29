fb38ad4f0f37383858063a418ded3a1c0f23cc37
Praveen Shinde <shindepraveen002@gmail.com>
2024-09-29 19:36:30 +0530

    Fix:small screen width for blind page

--- app/blind-75-leetcode/page.jsx
+++ app/blind-75-leetcode/page.jsx
@@ -6,7 +6,7 @@ import CustomAccordion from "@/components/custom-ui/CustomAccordion";
 
 const page = () => {
     return (
-        <div className="sm:w-10/12 lg:w-7/12 xl:w-5/12 sm:px-8 ">
+        <div className=" w-full sm:w-10/12 lg:w-7/12 xl:w-5/12 sm:px-8 ">
             <div>
                 <Link href={"/"} className="flex gap-2 items-center text-sm">
                     <RxArrowLeft />
